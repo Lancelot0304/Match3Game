14c1834d9305da1b22e8de1d6b4b94c5506a47ea
zhangpujun <zhangpujun@cmcm.com>
2020-11-03 15:26:41 +0800

    1

---
+++ web-desktop/src/settings.454a3.js
@@ -0,0 +1,24 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scenes/LoadingScene.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "6ad81",
+        resources: "d56a1",
+        main: "3fcf8"
+    }
+};
