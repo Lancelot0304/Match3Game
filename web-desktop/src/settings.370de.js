1f51fe1c3dd7f4c7eab98e0b85d1622564ece011
zhangpujun <zhangpujun@cmcm.com>
2020-11-03 15:14:37 +0800

    1

---
+++ web-desktop/src/settings.370de.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/LoadingScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"604e0",resources:"54311",main:"1a2a3"}};
