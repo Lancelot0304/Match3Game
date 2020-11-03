dcad8dd2e7b30a73a641cacd9385cec0580edeba
zhangpujun <zhangpujun@cmcm.com>
2020-11-03 15:32:28 +0800

    1

---
+++ web-mobile/src/settings.75cbf.js
@@ -0,0 +1,24 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scenes/LoadingScene.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "267e3",
+        resources: "7bfb2",
+        main: "5b2cf"
+    }
+};
